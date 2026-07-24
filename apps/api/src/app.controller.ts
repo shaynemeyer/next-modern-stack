6d97a9f9c048407c9538508d6a52843375901beb
Shayne Meyer <shayne.meyer@gmail.com>
2026-07-24 08:05:07 -0700

    wired up prisma to connect to a postgres database

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,17 +1,23 @@
 import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
+import { PrismaService } from "./prisma/prisma.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
-  @Get("health/check")
-  getHealth(): string {
-    return this.appService.getHealth();
+  @Get("health")
+  async getHealth() {
+    // return this.appService.getHealth();
+    await this.prismaService.$queryRaw`SELECT 1`;
+    return { status: "ok", db: "connected" };
   }
 }
---
+++ apps/api/src/prisma/prisma.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
+
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    const _result = await this.$queryRaw`SELECT 1`;
+  }
+}
