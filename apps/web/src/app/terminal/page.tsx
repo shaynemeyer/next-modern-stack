584d4af3ec824115fbb7c6cd624a7e624d427cfb
Shayne Meyer <shayne.meyer@gmail.com>
2026-07-24 17:11:51 -0700

    added fetch folders to the frontend

--- apps/web/src/app/terminal/page.tsx
+++ apps/web/src/app/terminal/page.tsx
@@ -1,10 +1,13 @@
 import { PageWrapper } from "@/modules/shared/page-wrapper";
 import { TerminalContent } from "@/modules/terminal/terminal-content";
 
-export default function TerminalPage() {
+export default async function TerminalPage() {
+  const folders = await fetch("http://localhost:3001/folders").then((res) =>
+    res.json(),
+  );
   return (
     <PageWrapper>
-      <TerminalContent />
+      <TerminalContent folders={folders} />
     </PageWrapper>
   );
 }
--- apps/web/src/modules/terminal/terminal-content.tsx
+++ apps/web/src/modules/terminal/terminal-content.tsx
@@ -1,9 +1,11 @@
-export function TerminalContent() {
-  const _a = 32;
-  const array = ["split", "the text", "into words"];
-  // array.flatMap((sentence) => sentence.split(" "));
-  array.flatMap((sentence) => sentence.split(" "));
+type Props = {
+  folders: {
+    id: number;
+    name: string;
+  }[];
+};
 
+export function TerminalContent({ folders }: Props) {
   return (
     <div className="flex justify-center flex-col w-full h-full">
       <div className="bg-gray-900 text-green-500 font-mono p-4 rounded w-full">
@@ -11,10 +13,9 @@ export function TerminalContent() {
           <div className="border-r border-gray-700 pr-4">
             <div className="border-b mb-2">Content Filters</div>
             <div>
-              <div>Item 1</div>
-              <div>Item 2</div>
-              <div>Item 3</div>
-              <div>Item 4</div>
+              {folders.map((folder) => (
+                <div key={folder.id}>{folder.name}</div>
+              ))}
             </div>
           </div>
         </div>
